c7bcb92a376f0afa8126dca1ecd7ea407641fc5e
pbcomm <pbcomm@gmail.com>
2010-08-28 19:51:03 -0700

    Streamlining things a bit.

--- jquery.editableText.js
+++ jquery.editableText.js
@@ -4,18 +4,13 @@
  * Copyright (c) 2009 Andris Valums, http://valums.com
  * Licensed under the MIT license (http://valums.com/mit-license/)
  */
-(function(){
-    /**
-     * The dollar sign could be overwritten globally,
-     * but jQuery should always stay accesible
-     */
-    var $ = jQuery;
+(function($){
 	/**
      * Extending jQuery namespace, we
      * could add public methods here
      */
 	$.editableText = {};
-    $.editableText.defaults = {		 
+    $.editableText.defaults = {
 		/**
 		 * Pass true to enable line breaks.
 		 * Useful with divs that contain paragraphs.
@@ -25,79 +20,71 @@
 		 * Event that is triggered when editable text is changed
 		 */
 		changeEvent : 'change'
-	};   		
+	};
 	/**
 	 * Usage $('selector).editableText(optionArray);
-	 * See $.editableText.defaults for valid options 
-	 */		
+	 * See $.editableText.defaults for valid options
+	 */
     $.fn.editableText = function(options){
-        var options = $.extend({}, $.editableText.defaults, options);
-        
-        return this.each(function(){
-             // Add jQuery methods to the element
-            var editable = $(this);
-            
-			/**
-			 * Save value to restore if user presses cancel
-			 */
-			var prevValue = editable.html();
-			
-			// Create edit/save buttons
-            var buttons = $(
-				"<div class='editableToolbar'>" +
-            		"<a href='#' class='edit'></a>" +
-            		"<a href='#' class='save'></a>" +
-            		"<a href='#' class='cancel'></a>" +
-            	"</div>")
-				.insertBefore(editable);
-			
-			// Save references and attach events            
-			var editEl = buttons.find('.edit').click(function() {
-				startEditing();
-				return false;
-			});							
-			
-			buttons.find('.save').click(function(){
-				stopEditing();
-				editable.trigger(options.changeEvent);
-				return false;
-			});
-						
-			buttons.find('.cancel').click(function(){
-				stopEditing();
-				editable.html(prevValue);
-				return false;
-			});		
-			
-			// Display only edit button			
-			buttons.children().css('display', 'none');
-			editEl.show();			
-			
-			if (!options.newlinesEnabled){
+        options = $.extend({}, $.editableText.defaults, options || {});
+
+        return this.each(function() {
+            // Add jQuery methods to the element
+            var
+                editable = $(this),
+
+    			/**
+	    		 * Save value to restore if user presses cancel
+		    	 */
+			    prevValue = editable.html(),
+
+    			// Create edit/save buttons
+                buttons = $(
+				    '<div class="editableToolbar">' +
+            		    '<a href="#" class="edit"/>' +
+                		'<a href="#" class="save"/>' +
+                		'<a href="#" class="cancel"/>' +
+                	'</div>').insertBefore(editable),
+
+    			// Save references and attach events
+			    editEl = buttons.find('.save').bind('click', { save: true }, stopEditing).end()
+                    .find('.cancel').bind('click', { save: false }, stopEditing).end()
+                    .children().css('display', 'none') // Display only edit button
+                    .find('.edit').bind('click', startEditing).show();
+
+			if (!options.newlinesEnabled) {
 				// Prevents user from adding newlines to headers, links, etc.
-				editable.keypress(function(event){
+				editable.keypress(function(event) {
 					// event is cancelled if enter is pressed
-					return event.which != 13;
+					return event.which !== 13;
 				});
 			}
-			
+
 			/**
 			 * Makes element editable
 			 */
-			function startEditing(){               
+			function startEditing(){
                 buttons.children().show();
                 editEl.hide();
-				                
 	            editable.attr('contentEditable', true);
+                return false;
 			}
 			/**
 			 * Makes element non-editable
 			 */
-			function stopEditing(){
+			function stopEditing(e) {
 				buttons.children().hide();
-				editEl.show();				
+				editEl.show();
                 editable.attr('contentEditable', false);
+
+			    if (e.data.save) {
+    				editable.trigger(options.changeEvent);
+			    } else {
+    				editable.html(prevValue);
+			    }
+
+                return false;
 			}
         });
     }
-})();
+})(jQuery);
